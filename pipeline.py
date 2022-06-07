a966b6b6b45e7a02233662c463e277d1936ec57b
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2022-06-07 23:49:03 +0700

    pipeline workflow

--- pipeline.py
+++ pipeline.py
@@ -3,7 +3,7 @@ from components.data_validation import validate_data
 from components.data_preparation import prepare_data
 from components.training import train
 
-project = 'mlops-fullpipeline'
+project = 'mlops'
 extract_data(project=project)
 validate_data(project=project)
 prepare_data(project=project)
